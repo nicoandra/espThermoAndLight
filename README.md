8be07f852f7fc9d6b8fe0c8f6e0dbd52246b64ac
Nicolas Andrade <nico@nmac.com.ar>
2016-11-11 19:04:21 -0500

    Adding library for Wifi setup

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # espThermoAndLight
+
+
+# Setup
+Turn on your ESP8266. Connect to the Wifi network it creates and setup the wifi network.
+It will reboot and Connect to the specified network.
+If it fails, it will set itself in Access-Point mode again.
--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
-/*
+/**
  * Blink
  * Turns on an LED on for one second,
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
 
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
+
+
 void setup()
 {
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  WiFiManager wifiManager;
+  // wifiManager.autoConnect("AP-NAME", "AP-PASSWORD");
+  wifiManager.autoConnect();
+
 }
 
 void loop()
@@ -16,9 +27,13 @@ void loop()
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
-  delay(2000);
+  delay(10);
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
-  delay(100);
+  delay(1000);
+
+
+  
+
 }
