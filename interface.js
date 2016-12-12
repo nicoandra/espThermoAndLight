6aef4a6a236b5448629465b1141278b06f102d6d
Nicolas Andrade <nico@nmac.com.ar>
2016-12-11 19:22:22 -0500

    Adding first DHT22

--- interface.js
+++ interface.js
@@ -5,10 +5,37 @@ const host = "192.168.1.128"
 server = dgram.createSocket('udp4');
 
 server.on('message', (msg, rinfo) => {
-		console.log("<<< " ,msg, rinfo);
+		stringMsg = msg.toString();
+		console.log("<<< " ,msg, stringMsg, rinfo);
 })
 
-payload = [0x01, 0xFF, 0xFF,0xFF];
-server.send(new Buffer(payload), 0, 4, port, host, function(err,res){
+resetMcu  = [0xFF, 0xFF, 0x00, 0xFF];
+pingBackWithHealthStatus = [0xFF, 0xFF, 0x00, 0x00];
+
+setTemperatureOfHeater1 = [0x11, 0x01, 0x18, 0x00];
+getTemperatureOfHeater1 = [0x10, 0x01, 0x00, 0x00];
+turnOnRelay1 = [0x21, 0x01, 0x00, 0x01];
+turnOffRelay1 = [0x21, 0x01, 0x00, 0x00];
+getStatusOfRelay1 = [0x20, 0x01, 0x00, 0x00];
+
+payload = pingBackWithHealthStatus;
+server.send(new Buffer(payload), 0, payload.length, port, host, function(err,res){
 	console.log(err, res);
 })
+
+
+
+
+/*
+Possible messages;
+
+BYTE
+0					1					2					3					Meaning
+11111111	11111111	00000000	11111111	Reset MCU
+11111111	11111111	00000000	00000000	Ping back for health status. A response will be sent with : UPTIME, IP.
+00010001	XXXXXXXX	yyyyyyyy  yyyyyyyy	Set temperature for heater in byte 1 to the value specified in byte 2 and 3. Desired temperature will be sent back as response.
+00010000	XXXXXXXX	00000000	00000000	Get temperature of heater in byte 1. Response will be sent back immediately and later on as well.
+00100001	XXXXXXXX	00000000	00000001	Turn ON  relay specified in byte 1. Response will be sent back immediately and later on as well.
+00100001	XXXXXXXX	00000000	00000000	Turn OFF  relay specified in byte 1. Response will be sent back immediately and later on as well.
+00100000	XXXXXXXX	00000000	00000000	Get status of relay specified in byte 1. Response will be sent back immediately and later on as well.
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -21,15 +21,30 @@ unsigned int localPort = 8888;
 WiFiUDP UDP;
 boolean udpConnected = false;
 char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet,
-char ReplyBuffer[] = "Acknowledged";
+// char ReplyBuffer[] = "Acknowledged";
+char ReplyBuffer[32];
 
 
 
 // Setup a DHT22 instance
-#define DHTPIN D2
+#define DHTLIVINGPIN D6
 #define DHTTYPE DHT22
-DHT_Unified dht(DHTPIN, DHTTYPE);
+DHT_Unified dhtLiving(DHTLIVINGPIN, DHTTYPE);
+float temperatures[2] = {0,0};
+float humidity[2] = {-1,-1};
+float temporaryData[2] = {-1, -1};
+uint32_t delayMS;
 
+
+// Setup the time tracking variable
+unsigned long time = millis();
+unsigned timeOfLastTemperatureRead = time;
+
+// Setup the button reader
+int ButtonState      = 0;
+int PrevButtonState = 0;
+unsigned long TimeWhenButtonWasPressed = millis();
+bool IsALongPress = false;
 
 boolean connectUDP(){
   boolean state = false;
@@ -47,12 +62,86 @@ boolean connectUDP(){
   return state;
 }
 
-void listenUdp(){
+void monitorButton(){
+  ButtonState = digitalRead(D2);
+  if(ButtonState == 0 && PrevButtonState == 0){
+    // Serial.println("BUTTON: No changes");
+    return ;
+  }
 
+  if(ButtonState == 1 && PrevButtonState == 0){
+    Serial.println("BUTTON: PRESSED");
+    TimeWhenButtonWasPressed = millis();
+    PrevButtonState = ButtonState;
+    return ;
+  }
+
+  if(ButtonState == 0 && PrevButtonState == 1){
+    if(IsALongPress){
+      Serial.println("BUTTON: RELEASED AFTER LONG PRESS. IGNORE.");
+    } else {
+      Serial.println("BUTTON: RELEASED AFTER SHORT PRESS. ACTION!");
+    }
+    IsALongPress = false;
+    TimeWhenButtonWasPressed = 0;
+    PrevButtonState = ButtonState;
+    return ;
+  }
+
+  if(!IsALongPress && ButtonState == 1 && PrevButtonState == 1 && TimeWhenButtonWasPressed + 1500 < millis()){
+    Serial.println("BUTTON: STILL IN LONG PRESS");
+    IsALongPress = true;
+    TimeWhenButtonWasPressed = millis();
+    return ;
+  }
+
+}
+
+
+
+void monitorButtonBackup(){
+  ButtonState = digitalRead(D2);
+  if(ButtonState != PrevButtonState){
+    Serial.print("BUTTON: ");
+    Serial.print(PrevButtonState);
+    Serial.print(" ");
+    Serial.println(ButtonState);
+
+    if(ButtonState == 1){
+      TimeWhenButtonWasPressed = millis();
+    }
+
+    if (ButtonState == 0 && TimeWhenButtonWasPressed + 1500 > millis()) {
+      Serial.println("That was a SHORT press");
+      TimeWhenButtonWasPressed = 0;
+    }
+    PrevButtonState = ButtonState;
+
+  } else {
+
+    if(ButtonState == 1 && PrevButtonState == 1 && TimeWhenButtonWasPressed + 1500 < millis()){
+      Serial.println("That was a LOOOONG press right there");
+      ButtonState = 1;
+      PrevButtonState = 0;
+      TimeWhenButtonWasPressed = millis();
+    }
+
+  }
+  return ;
+}
+
+void sendUdpResponse(char ReplyBuffer[]){
+  UDP.beginPacket(UDP.remoteIP(), UDP.remotePort());
+  UDP.write(ReplyBuffer);
+  UDP.endPacket();
+}
+
+
+void listenUdp(){
   // if there’s data available, read a packet
   int packetSize = UDP.parsePacket();
-  Serial.print("Packetsize: ");
-  Serial.println(packetSize);
+  // Serial.print("Packetsize: ");
+  // Serial.println(packetSize);
 
   if(packetSize){
     Serial.println("");
@@ -72,6 +161,39 @@ void listenUdp(){
 
     // read the packet into packetBufffer
     UDP.read(packetBuffer,UDP_TX_PACKET_MAX_SIZE);
+
+    if(packetSize != 4){
+      sendUdpResponse((char *)"Unknown instruction");
+      return ;
+    }
+
+    switch(packetBuffer[0]){
+      case 0xFF:
+        if (packetBuffer[1] != 0xFF){
+          sendUdpResponse((char *)"Commands with 0xFF must continue with 0xFF as well");
+          return;
+        }
+
+        if (packetBuffer[2] != 0x00){
+          sendUdpResponse((char *)"Commands with 0xFF 0xFF must continue with 0x00");
+          return;
+        }
+
+        switch(packetBuffer[3]){
+          case 0x00: sendUdpResponse((char *)"PingBack!"); Serial.println("PingBack!"); break;
+          case 0xFF: sendUdpResponse((char *)"ResetDevice!"); Serial.println("ResetDevice!"); break;
+          default: sendUdpResponse((char *)"Last byte should be either 0x00 or 0xFF"); break;
+        }
+        return ;
+
+      case 0x11: break; // Set temperature of heater in position [byte 2
+      case 0x10: break; // Get temperature of heater in position [byte 2
+
+      break;
+      default:  Serial.println("EEEEEH BEN ...(??)"); break;
+    }
+
+
     Serial.println("Contents:");
     for(int i = 0; i < packetSize; i++){
       int value = packetBuffer[i];
@@ -95,11 +217,25 @@ void listenUdp(){
     // turn LED on or off depending on value recieved
     // digitalWrite(5,value);
   }
-  delay(10);
-
 }
 
 
+void ledOn(int pinNumber){
+  if(LED_BUILTIN == pinNumber){
+    digitalWrite(pinNumber, 0);
+    return ;
+  }
+  digitalWrite(pinNumber, 1);
+}
+
+
+void ledOff(int pinNumber){
+  if(LED_BUILTIN == pinNumber){
+    digitalWrite(pinNumber, 1);
+    return ;
+  }
+  digitalWrite(pinNumber, 0);
+}
 
 
 
@@ -109,20 +245,23 @@ void setup()
   Serial.begin(115200);
   Serial.println("Booting...");
 
-
-  dht.begin();
-
+  // Start temperature heaters
+  dhtLiving.begin();
 
 
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
+  ledOff(LED_BUILTIN);
   pinMode(D7, OUTPUT);
+  pinMode(D2, INPUT); // For the button
+  digitalWrite(D2, HIGH);
 
+
+  // Network settings
   WiFiManager wifiManager;
   // wifiManager.autoConnect("AP-NAME", "AP-PASSWORD");
   wifiManager.autoConnect();
   Serial.println("Booted");
-
 
   if(connectUDP()) {
     Serial.println("Listening UDP");
@@ -132,7 +271,7 @@ void setup()
 
 
   sensor_t sensor;
-  dht.temperature().getSensor(&sensor);
+  dhtLiving.temperature().getSensor(&sensor);
   Serial.println("------------------------------------");
   Serial.println("Temperature");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
@@ -143,7 +282,7 @@ void setup()
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" *C");
   Serial.println("------------------------------------");
   // Print humidity sensor details.
-  dht.humidity().getSensor(&sensor);
+  dhtLiving.humidity().getSensor(&sensor);
   Serial.println("------------------------------------");
   Serial.println("Humidity");
   Serial.print  ("Sensor:       "); Serial.println(sensor.name);
@@ -154,15 +293,70 @@ void setup()
   Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println("%");
   Serial.println("------------------------------------");
   // Set delay between sensor readings based on sensor details.
+  delayMS = sensor.min_delay / 1000;
 
+
+  time = millis();
+}
+
+
+
+void readTemp(DHT_Unified dht, float temporaryData[]){
+  sensors_event_t event;
+    dht.temperature().getEvent(&event);
+    if (isnan(event.temperature)) {
+      // Serial.println("Error reading temperature!");
+      temporaryData[0] = -999999;
+      temporaryData[1] = -999999;
+    } else {
+      // Serial.print("Temperature: ");
+      Serial.print(event.temperature);
+      temporaryData[0] = event.temperature;
+      // Serial.println(" *C");
+    }
+
+    // Get humidity event and print its value.
+    dht.humidity().getEvent(&event);
+    if (isnan(event.relative_humidity)) {
+      // Serial.println("Error reading humidity!");
+      temporaryData[1] = -999999;
+    }  else {
+      // Serial.print("Humidity: ");
+      Serial.print(event.relative_humidity);
+      temporaryData[1] = event.relative_humidity;
+      Serial.println("%");
+    }
 
 }
 
 void loop()
 {
 
+  time = millis();
   listenUdp();
 
+  if(timeOfLastTemperatureRead > time){
+    // Every around 50 days, millis() will reset itself. This catches that situation.
+    timeOfLastTemperatureRead = 0;
+  }
+
+  if(timeOfLastTemperatureRead + 15000 < time){
+    ledOn(D7);
+    // If the last read was done more than 2 seconds ago, read!
+    readTemp(dhtLiving, temporaryData);
+    timeOfLastTemperatureRead = millis();
+    Serial.print("Read temperature is: ");
+    Serial.println(temporaryData[0]);
+
+    Serial.print("Read humidity is: ");
+    Serial.println(temporaryData[1]);
+    ledOff(D7);
+  }
+
+  monitorButton();
+
+
+/*
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // digitalWrite(D7, HIGH);
@@ -176,5 +370,6 @@ void loop()
   digitalWrite(LED_BUILTIN, HIGH);
    // wait for a second
   delay(950);
+  */
 
 }
